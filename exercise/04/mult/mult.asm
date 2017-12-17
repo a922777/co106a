5a47c92cebb4d361ef75d14151de737c42d9797d
s110510548 <31913858+a922777@users.noreply.github.com>
2017-12-17 20:52:51 +0800

    Add files via upload

--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@0  
+D=M  
+@2  
+M=0  
+(LOOP)  
+@1  
+D=M  
+@END  
+D;JEQ  
+  
+@1  
+M=M-1  
+  
+@0  
+D=M  
+@2  
+M=M+D  
+  
+@1  
+D=M  
+@LOOP  
+D;JGT  
+  
+@END  
+(END)  
+0;JMP  
